88b7c124f37e0766f13b8bcd4c8e8f4bb3a9218c
RohithGadamsetty <ro93939@gmail.com>
2021-07-10 15:56:34 +0530

    dynamic port changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OauthComponent } from './oauth/oauth.component';
-import { CallbackComponent } from './callback/callback.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { OauthComponent } from "./oauth/oauth.component";
+import { CallbackComponent } from "./callback/callback.component";
 
 import {
   NbAuthModule,
   NbOAuth2AuthStrategy,
   NbOAuth2ResponseType,
-} from '@nebular/auth';
-import { NbCardModule, NbIconLibraries, NbIconModule, NbLayoutComponent, NbLayoutModule, NbThemeModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { HttpClientModule } from '@angular/common/http';
-import { ProfileComponent } from './profile/profile.component';
+} from "@nebular/auth";
+import {
+  NbCardModule,
+  NbIconLibraries,
+  NbIconModule,
+  NbLayoutModule,
+  NbThemeModule,
+} from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { HttpClientModule } from "@angular/common/http";
+import { ProfileComponent } from "./profile/profile.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     OauthComponent,
     CallbackComponent,
-    ProfileComponent,
-    // NbLayoutComponent
-    // NbIconLibraries
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -31,17 +35,19 @@ import { ProfileComponent } from './profile/profile.component';
     NbAuthModule.forRoot({
       strategies: [
         NbOAuth2AuthStrategy.setup({
-          name: 'google',
-          clientId: '273826063453-bmeb56dvbt0b3r6eo1cq94guafl1hgj3.apps.googleusercontent.com',
-          clientSecret: '6iYbfdwiT-a12vrdpAwZqRFB',
+          name: "google",
+          clientId:
+            "273826063453-bmeb56dvbt0b3r6eo1cq94guafl1hgj3.apps.googleusercontent.com",
+          clientSecret: "6iYbfdwiT-a12vrdpAwZqRFB",
           authorize: {
-            endpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+            endpoint: "https://accounts.google.com/o/oauth2/v2/auth",
             responseType: NbOAuth2ResponseType.TOKEN,
-            scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
-            redirectUri: 'http://localhost:4200/callback',
+            scope:
+              "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
+            redirectUri: `http://localhost:${window.location.port}/callback`,
           },
           redirect: {
-            success: '/auth',
+            success: "/auth",
           },
         }),
       ],
@@ -51,9 +57,9 @@ import { ProfileComponent } from './profile/profile.component';
     NbCardModule,
     NbLayoutModule,
     HttpClientModule,
-    NbThemeModule.forRoot()
+    NbThemeModule.forRoot(),
   ],
   providers: [NbIconLibraries],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
